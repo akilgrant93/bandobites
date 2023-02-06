24c50edf03896d988f70b43cd3633ad975eee901
Akil Grant <akil.grant.93@gmail.com>
2023-02-06 18:59:14 -0500

    navbar conversion

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,8 @@ import React, {useState, useEffect, useRef} from 'react'
 import Link from 'next/link'
 import Cart from './Cart'
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { SiJordan, SiNike, SiAdidas } from "react-icons/si";
+import { FaCannabis } from "react-icons/fa";
+import { GiMushroom } from "react-icons/gi";
 export default function NavBar(props) {
 
   const [isOpen, setIsOpen] = useState(false);
@@ -26,16 +27,22 @@ export default function NavBar(props) {
   const buttonStyle01 = {padding:5, marginLeft: 10, marginRight:10, fontSize:12}
   const buttonStyle02 = {padding:10, fontSize:12}
 
+  function MouseOver(event) {
+    event.target.style.color = 'red';
+  }
+  function MouseOut(event){
+    event.target.style.color="";
+  }
+
   return (
     <div>
     <ul className='shadow-md flex' style={props.windowDimensions.width < 1024 ? objectStyle01 : objectStyle02}>
-      <li style={{padding:5}}><Link href="/"><img style={{height: 35}} src={'/shoelala.png'} alt={'Shoelala logo'}/></Link></li>
+      <li style={{padding:5}}><Link href="/"><img style={{height: 35}} src={'/bando bites.png'} alt={'Shoelala logo'}/></Link></li>
       <div style={props.windowDimensions.width < 1024 ? tabStyle01 : tabStyle02}>
-      <li style={props.windowDimensions.width < 1024 ? buttonStyle01 : buttonStyle02}><Link href="/">ALL</Link></li>
-      <li style={props.windowDimensions.width < 1024 ? buttonStyle01 : buttonStyle02}><Link href="/jordan"><SiJordan className='mx-1' size={18}/></Link></li>
-      <li style={props.windowDimensions.width < 1024 ? buttonStyle01 : buttonStyle02}><Link href="/adidas"><SiAdidas className='mx-1' size={18}/></Link></li>
-      <li style={props.windowDimensions.width < 1024 ? buttonStyle01 : buttonStyle02}><Link href="/nike"><SiNike className='mx-1' size={18}/></Link></li>
-      <li className='font-semibold cursor-pointer' onClick={() => {setIsOpen(true)}} style={props.windowDimensions.width < 1024 ? buttonStyle01 : buttonStyle02}><AiOutlineShoppingCart className='mx-1' size={18}/></li>
+      <li onMouseOver={MouseOver} onMouseOut={MouseOut} style={props.windowDimensions.width < 1024 ? buttonStyle01 : buttonStyle02}><Link href="/">ALL</Link></li>
+      <li style={props.windowDimensions.width < 1024 ? buttonStyle01 : buttonStyle02}><Link href="/adidas"><FaCannabis onMouseOver={MouseOver} onMouseOut={MouseOut} className='mx-1' size={18}/></Link></li>
+      <li style={props.windowDimensions.width < 1024 ? buttonStyle01 : buttonStyle02}><Link href="/nike"><GiMushroom onMouseOver={MouseOver} onMouseOut={MouseOut} className='mx-1' size={18}/></Link></li>
+      <li className='font-semibold cursor-pointer' onClick={() => {setIsOpen(true)}} style={props.windowDimensions.width < 1024 ? buttonStyle01 : buttonStyle02}><AiOutlineShoppingCart className='mx-1' onMouseOver={MouseOver} onMouseOut={MouseOut} size={18}/></li>
       {/* <li className='font-semibold' style={{padding:5, fontSize:12}}><Link href="/">ACCOUNT</Link></li> */}
       </div>
     </ul>
