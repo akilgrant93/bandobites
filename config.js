6186637a07494ce1a3d6e33ed2d9f349bcb7b6ed
Akil Grant <akil.grant.93@gmail.com>
2023-02-06 21:16:19 -0500

    rerouted firebase to actually target bando bites db

--- config.js
+++ config.js
@@ -3,15 +3,16 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyCZz0HlbvboMDvOD-65MzncE6P-hgtnuno",
-  authDomain: "shoelala-a2ebf.firebaseapp.com",
-  projectId: "shoelala-a2ebf",
-  storageBucket: "shoelala-a2ebf.appspot.com",
-  messagingSenderId: "1095236202408",
-  appId: "1:1095236202408:web:0c65b4392aa0fce68cf79d",
-  measurementId: "G-N0ZW9DP0TR"
+const firebaseConfig = {
+  apiKey: "AIzaSyDv-VEWKXg2vCZjdWg6C7w0lXbvw4J7fnQ",
+  authDomain: "bando-bites.firebaseapp.com",
+  projectId: "bando-bites",
+  storageBucket: "bando-bites.appspot.com",
+  messagingSenderId: "7575103153",
+  appId: "1:7575103153:web:34c20330ea55aed60b2490",
+  measurementId: "G-S7EDH70279"
 };
+
 
 // Initialize Firebase
 if(!firebase.apps.length){
